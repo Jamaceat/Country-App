dfc65323481fdb8558ad204e9c07d577ae2b470f
Johan <johanoriginalplus@gmail.com>
2024-02-19 17:37:12 -0500

    Routing Children

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPagesComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [HomePageComponent, AboutPageComponent, SidebarComponent],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPagesComponent,
+  ],
 })
 export class SharedModule {}
